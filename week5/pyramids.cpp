7be2aab291e4807706148b86e08c58094bdee9bd
0xA2 <36355541+0xA2@users.noreply.github.com>
2020-11-03 22:54:19 +0000

    Update pyramids.cpp

--- week5/pyramids.cpp
+++ week5/pyramids.cpp
@@ -2,24 +2,24 @@
 #include <climits>
 
 int main(){
-	long long int height, missing;
+	int height, missing;
 	std::cin >> height;
 	std::cin >> missing;
-	long long int pyramid[height+1][height+1] = {};
-	long long int missingInRow, missingInCol;
-	for(long long int i = 0; i<missing; i++){
+	int pyramid[height+1][height+1] = {};
+	int missingInRow, missingInCol;
+	for(int i = 0; i<missing; i++){
 		std::cin >> missingInRow;
 		std::cin >> missingInCol;
 		pyramid[((height-missingInRow)%height)+1][missingInCol] = -1;
 	}
-	long long int paths = 0;
+	int paths = 0;
 	if(pyramid[1][1] == -1) { 
 		std::cout << 0 << std::endl;
 		return 0;
 	}
 	pyramid[1][1] = 1;
-	for(long long int i = 1; i<=height; i++){
-		for(long long int j = 1; j<=i; j++){
+	for(int i = 1; i<=height; i++){
+		for(int j = 1; j<=i; j++){
 			if(pyramid[i][j] == -1){ continue;}
 			if(pyramid[i-1][j-1] != -1){
 				pyramid[i][j] += pyramid[i-1][j-1];
